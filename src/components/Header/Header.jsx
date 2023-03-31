de8bf7660dddc3f023de70613891e51e8938f4b5
Iftekher Aziz <azizchy02@gmail.com>
2023-03-31 18:44:46 +0600

    Create fake data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <>
       <nav className="navbar navbar-expand-md bg-body-tertiary mt-3">
         <div className="container ">
-          <div className='px-md-5'>
-            <a className="navbar-brand fs-2 fw-bolder" href="#">Learning Cafe</a>
+          <div>
+            <a className="navbar-brand fs-2 fw-bolder" href="#">
+              Learning Cafe
+            </a>
           </div>
-          <div className='px-md-5'>
-            <div className="collapse navbar-collapse d-md-flex justify-content-md-end gap-3" id="navbarNav">
-              <div>
-                < img src='./profile.png' className="card-img-top rounded-circle" alt='Profile'
-                  style={{ height: "50px", width: "50px" }} />
-              </div>
+          <div
+            className="collapse navbar-collapse d-md-flex justify-content-md-end gap-3"
+            id="navbarNav"
+          >
+            <div>
+              <img
+                src="./profile.png"
+                className="card-img-top rounded-circle"
+                alt="Profile"
+                style={{ height: "50px", width: "50px" }}
+              />
             </div>
           </div>
         </div>
       </nav>
-      <div className='container'>
+      <div className="container">
         <hr />
       </div>
     </>
